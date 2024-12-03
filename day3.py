6a89888891688d845ad7efb20c020f26463443e8
Kevin Tower <kevin.tower@disney.com>
2024-12-03 17:04:54 -0500

    Day 3 part 2

--- day3.py
+++ day3.py
@@ -1,6 +1,8 @@
 import re
 
 MUL_RE = re.compile(r"mul\((\d+),(\d+)\)")
+DO_RE = re.compile(r"do\(\)")
+DONT_RE = re.compile(r"don't\(\)")
 
 def tuple_to_int_and_multiply(t_list: tuple[int|str, int|str]) -> int:
     return int(t_list[0]) * int(t_list[1])
@@ -14,9 +16,30 @@ def part_1(memDump: str) -> int:
         opTotal += tuple_to_int_and_multiply(op)
     return opTotal
 
+def part_2(memDump: str) -> int:
+    enabled = True  # Start with the mul operation enabled
+    opTotal = 0
+    cur_string = ""
+
+    for char in memDump:
+        cur_string += char
+        if DO_RE.findall(cur_string):
+            enabled = True
+            cur_string = ""
+        elif DONT_RE.findall(cur_string):
+            enabled = False
+            cur_string = ""
+        elif opMatch := MUL_RE.findall(cur_string):
+            if enabled:
+                for op in opMatch:
+                    opTotal += tuple_to_int_and_multiply(op)
+            cur_string = ""
+    return opTotal
+
 
 if __name__ == "__main__":
     memDumpFile = open("day3-input.txt", "r")
     memDump = memDumpFile.read()
     print(part_1(memDump))
+    print(part_2(memDump))
     memDumpFile.close()
