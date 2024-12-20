fd6374b8f51cc4ba1ead598268c248994a5f8223
Kevin Tower <kevin.tower@disney.com>
2024-12-20 10:38:42 -0500

    Partial implementation of day 4 part 2

--- day4.py
+++ day4.py
@@ -43,6 +43,25 @@ def find_target_string(source: list[str], target: str, target_index: int, cur_x:
     # If we get here then we don't have a match at this location in any direction
     return False
 
+def check_next_char(source: list[str], cur_x: int, cur_y: int, dir: Direction, query: str) -> bool:
+    """Check the next character in the specified direction and return True if it matches the query string"""
+    if dir == Direction.UP and cur_x > 0:
+        return source[cur_x - 1][cur_y] == query
+    elif dir == Direction.UP_RIGHT and cur_x > 0 and cur_y < len(source[cur_x - 1]) - 1:
+        return source[cur_x - 1][cur_y + 1] == query
+    elif dir == Direction.RIGHT and cur_y < len(source[cur_x]) - 1:
+        return source[cur_x][cur_y + 1] == query
+    elif dir == Direction.DOWN_RIGHT and cur_x < len(source) - 1 and cur_y < len(source[cur_x + 1]) - 1:
+        return source[cur_x + 1][cur_y + 1] == query
+    elif dir == Direction.DOWN and cur_x < len(source) - 1:
+        return source[cur_x + 1][cur_y] == query
+    elif dir == Direction.DOWN_LEFT and cur_x < len(source) - 1 and cur_y > 0:
+        return source[cur_x + 1][cur_y - 1] == query
+    elif dir == Direction.LEFT and cur_y > 0:
+        return source[cur_x][cur_y - 1] == query
+    elif dir == Direction.UP_LEFT and cur_x > 0 and cur_y > 0:
+        return source[cur_x - 1][cur_y - 1] == query
+    return False
 
 if __name__ == "__main__":
     wordSearchFile = open("day4-input.txt", "r")
@@ -57,5 +76,13 @@ if __name__ == "__main__":
             for dir in Direction:
                 if find_target_string(WORDSEARCH_SOURCE, targetString, 0, x, y, dir):
                     targetFindCount += 1
+    
+    # Part 2: Find all instances of "MAS" in the form of an X
+    targetString = "MAS"
+    xTargetFindCount = 0
+
+    for x in range(len(WORDSEARCH_SOURCE)):
+        for y in range(len(WORDSEARCH_SOURCE[x])):
+            xTargetFindCount = 0
     
     print(f"Part 1 - Number of 'XMAS' instances in puzzle: {targetFindCount}")
